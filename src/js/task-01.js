7bbdfed741893c9c3e6bdab76352106ad061517e
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-17 18:51:31 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,17 @@
-console.log("Hello");
 
 
-// const countEl = document.querySelectorAll(item);
-// console.log(countEl);
+const itemEl = document.querySelectorAll("#categories .item");
 
+console.log(`Number of categories: ${itemEl.length}`);
+// ***************************************************************// 
+itemEl.forEach((elem, i) => {
+
+    const title = elem.querySelector("h2");    
+       
+    console.log("");
+    console.dir(title.textContent);
+    
+    const liItemEl = elem.querySelectorAll("li");
+
+    console.dir(`Element: ${liItemEl.length}`);
+});
