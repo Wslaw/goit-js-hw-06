d6856c7df640fb4f08cd75b2cf652499066533cc
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-19 13:16:12 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients")
+const arr = [];
+
+ingredients.map((ingredient) => {  
+  const newItemEl = (document.createElement("li"));
+  newItemEl.textContent = ingredient; 
+  newItemEl.classList.add("item"); 
+  arr.push(newItemEl);
+  
+})
+
+list.append(...arr);  
