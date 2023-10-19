2ae42940c4c3cc72e8739c8a7a423d9d08fe9e4c
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-19 13:49:44 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,16 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
+// *********************************************
+// ------------Variant 2------------------------
+// 
+// 
 const list = document.querySelector("#ingredients")
-const arr = [];
 
-ingredients.map((ingredient) => {  
-  const newItemEl = (document.createElement("li"));
-  newItemEl.textContent = ingredient; 
-  newItemEl.classList.add("item"); 
-  arr.push(newItemEl);
-  
-})
+ const liElements = ingredients.map((ingredient) => 
+   `<li class = "item"> ${ingredient}</li>`
+ );
+list.insertAdjacentHTML("beforeend", liElements.join(''));
 
-list.append(...arr);  
+
+// *********************************************
+// ------------Variant 1------------------------
+// 
+// 
+// const list = document.querySelector("#ingredients");
+// const arr = [];
+
+// ingredients.map((ingredient) => {
+//   const newItemEl = (document.createElement("li"));
+//   console.log(newItemEl);
+//   newItemEl.textContent = ingredient;
+//   newItemEl.classList.add("item");
+//   arr.push(newItemEl);
+// });
+// list.append(...arr);
