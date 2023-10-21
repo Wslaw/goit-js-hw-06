478acb13526e8a372294d344a26cd3ea7f5f71bb
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-21 20:32:49 +0300

    Update

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,8 +1,7 @@
 const textInput = document.getElementById("font-size-control");
 const textOutput = document.getElementById("text");
 
-textInput.addEventListener("input", () => {       
-   
-    textOutput.style.fontSize = textInput.value + "px";
-   
+textInput.addEventListener("input", () => {
+  textOutput.style.fontSize = textInput.value + "px";
+  textOutput.style.opacity = textInput.value / 100;
 });
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,27 @@
+const input = document.querySelector(".login-form");
+const btn = document.querySelector("button")
+let email = "";
+let password = "";
+input.addEventListener("input", handleInput);
+btn.addEventListener("click", handleBtn);
 
+function handleInput(event) {
+   
+    email = event.currentTarget[0].value;
+    password = event.currentTarget[1].value;
+
+    console.dir( email);
+    console.dir(password);
+};
+function handleBtn(event) {
+    event.preventDefault();
+    if (email && password) {
+        console.log("true");
+    } else {
+        console.log("false");
+    alert("все поля должны быть заполнены");
+    };
+    const resultObjectField = {};
+
+
+}
