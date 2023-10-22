5cdcb9dd39590c76e038b322da7c771b6a263b63
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-22 22:34:34 +0300

    update-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,24 @@
+// ************************************
+console.log('Hello!');
+const inputNumber = document.querySelector('#controls');
+const btnCreate = document.querySelector('[data - create]');
+const btnDestroy = document.querySelector('[data - destroy]');
+
+inputNumber.addEventListener('input', handleClick);
+btnCreate.addEventListener('click', handleCreateClick);
+btnDestroy.addEventListener('click', handleDestroyClick);
+
+function handleClick() {
+  console.log(inputNumber.value);
+}
+
+function handleCreateClick() {
+  console.log('click', btnCreate.value);
+}
+
+function handleDestroyClick() {
+  console.log('click', btnDestroy.value);
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
