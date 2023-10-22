484b845b5838a952fdc8cbe04bbc9bed2f500d1e
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-23 02:15:36 +0300

    95%

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -10,29 +10,27 @@ btnCreate.addEventListener("click", handleCreateClick);
 btnDestroy.addEventListener('click', handleDestroyClick);
 
 let amount = 0;
-let arr = [];
-let el = '';
-let unic = 30;
+
 
 function createBoxes(amount) {
-  for (let i = 0; i <= amount; i+=1) {
-    el = `<divl class = box  
-    padding: 10px;
-    background-color: #212121;    
-    border: 1px
-    width: ${unic} + 10 * ${i} + px;
-    height: ${unic} + 10 * ${i} + px;
-    >Send</divl>`;
+  let arr = [];
+  for (let i = 1; i <= amount; i+=1) {
+    const el = `<div class = "box"  style ="
+        background-color: ${getRandomHexColor()};    
+    border: 1px solid #000;
+    width: ${20 + 10 * i}px;
+    height: ${20 + 10 * i}px;"
+    ></div>`;
     arr.push(el)
    
   }
-  divBoxes.innerHTML = arr.join();
+  divBoxes.innerHTML = arr.join('');
 
 }
 
 
 function handleClick() {
-  amount = inputNum.value;
+  amount = parseInt(inputNum.value);
 }
 
 function handleCreateClick() {
@@ -41,12 +39,26 @@ function handleCreateClick() {
   }
   
   function handleDestroyClick() {
-      // console.log("click");
-    console.log(arr.join(' '));
-  // console.log("btnDestroy");
+destroyBoxes();
 }
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+    
+function destroyBoxes() {
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach(box => {
+    if (box.innerHTML === '') {
+      box.remove();
+  }
+})
+    
+
+    
+   
+  }
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
