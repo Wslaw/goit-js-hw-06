8b5a3e3c85543f5c2c21ea3ff962f4225a56bd40
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-22 23:01:41 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,8 +1,8 @@
 // ************************************
 console.log('Hello!');
-const inputNumber = document.querySelector('#controls');
-const btnCreate = document.querySelector('[data - create]');
-const btnDestroy = document.querySelector('[data - destroy]');
+const inputNumber = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
 
 inputNumber.addEventListener('input', handleClick);
 btnCreate.addEventListener('click', handleCreateClick);
@@ -10,17 +10,21 @@ btnDestroy.addEventListener('click', handleDestroyClick);
 
 function handleClick() {
   console.log(inputNumber.value);
+  console.log("click");
 }
 
 function handleCreateClick() {
-  console.log('click', btnCreate.value);
+  // console.log("click");
+  // btnCreate.style.backgroundColor = "#336688";
+  console.log("btnCreate");
 }
 
 function handleDestroyClick() {
-  console.log('click', btnDestroy.value);
+  // console.log("click");
+  console.log("btnDestroy");
 }
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
