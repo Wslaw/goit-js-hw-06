41e88ab7b8a61c0af9434a3a9a31f245efe63dd0
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-22 14:02:37 +0300

    Update 9

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -3,5 +3,8 @@ const textOutput = document.getElementById("text");
 
 textInput.addEventListener("input", () => {
   textOutput.style.fontSize = textInput.value + "px";
-  textOutput.style.opacity = textInput.value / 100;
+  textOutput.style.opacity = 1 - textInput.value / 100;
+  // textOutput.style.opacity = 1 - parseFloat(textInput.value / 100);
+  // textOutput.style.opacity = 1 - Number(textInput.value / 100);
+  // console.log((typeof(textInput.value)));
 });
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,4 +2,16 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+  }
+
+
+const body = document.querySelector("body");
+const changeColor = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+changeColor.addEventListener("click", () => {
+  const currentColor = getRandomHexColor();
+  body.style.backgroundColor = currentColor;
+  span.textContent = "- " + currentColor;
+});
+
