c8d1c760d96d71d75658d03fe9d86f3d550a80ff
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-23 16:23:07 +0300

    99.5%

--- js/task-02.js
+++ js/task-02.js
@@ -10,26 +10,26 @@ const ingredients = [
 // ------------Variant 2------------------------
 // 
 // 
-const list = document.querySelector("#ingredients")
+// const list = document.querySelector("#ingredients")
 
- const liElements = ingredients.map((ingredient) => 
-   `<li class = "item"> ${ingredient}</li>`
- );
-list.insertAdjacentHTML("beforeend", liElements.join(''));
+//  const liElements = ingredients.map((ingredient) => 
+//    `<li class = "item"> ${ingredient}</li>`
+//  );
+// list.insertAdjacentHTML("beforeend", liElements.join(''));
 
 
 // *********************************************
 // ------------Variant 1------------------------
 // 
 // 
-// const list = document.querySelector("#ingredients");
-// const arr = [];
+const list = document.querySelector("#ingredients");
+const arr = [];
 
-// ingredients.map((ingredient) => {
-//   const newItemEl = (document.createElement("li"));
-//   console.log(newItemEl);
-//   newItemEl.textContent = ingredient;
-//   newItemEl.classList.add("item");
-//   arr.push(newItemEl);
-// });
-// list.append(...arr);
+ingredients.map((ingredient) => {
+  const newItemEl = (document.createElement("li"));
+  console.log(newItemEl);
+  newItemEl.textContent = ingredient;
+  newItemEl.classList.add("item");
+  arr.push(newItemEl);
+});
+list.append(...arr);
--- js/task-05.js
+++ js/task-05.js
@@ -1,22 +1,22 @@
 // *************Variant 2********************************
-// const nameInput = document.getElementById("name-input");
-// const nameOutput = document.getElementById("name-output");
-
-// nameInput.addEventListener("input", () => {
-//     console.log(nameInput.value);
-//     if (nameInput.value.trim() === '') {
-//         nameOutput.textContent = "Anonymous";
-//     } else {
-//         nameOutput.textContent = nameInput.value;
-
-//     }
-// })
-// ***********Variant 1*********************************
 const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById("name-output");
 
 nameInput.addEventListener("input", () => {
-    nameOutput.textContent = nameInput.value.trim() === "" ?
-        alert("Все поля должны быть заполнены!") : continue;
-});
+    console.log(nameInput.value);
+    if (nameInput.value.trim() === '') {
+        nameOutput.textContent = "Anonymous";
+    } else {
+        nameOutput.textContent = nameInput.value;
+
+    }
+})
+// ***********Variant 1*********************************
+// const nameInput = document.getElementById("name-input");
+// const nameOutput = document.getElementById("name-output");
+
+// nameInput.addEventListener("input", () => {
+//     nameOutput.textContent = nameInput.value.trim() === "" ?
+//         alert("Все поля должны быть заполнены!") : continue;
+// });
 
