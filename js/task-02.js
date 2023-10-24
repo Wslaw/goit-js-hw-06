34bccf60abcf6ab04961fc5a390e5f1bb03031ac
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-24 19:11:31 +0300

    1

--- js/task-02.js
+++ js/task-02.js
@@ -1,32 +1,24 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 // *********************************************
 // ------------Variant 2------------------------
-// 
-// 
+//
+//
 // const list = document.querySelector("#ingredients")
 
-//  const liElements = ingredients.map((ingredient) => 
+//  const liElements = ingredients.map((ingredient) =>
 //    `<li class = "item"> ${ingredient}</li>`
 //  );
 // list.insertAdjacentHTML("beforeend", liElements.join(''));
 
-
 // *********************************************
 // ------------Variant 1------------------------
-// 
-// 
+//
+//
 const list = document.querySelector("#ingredients");
 const arr = [];
 
 ingredients.map((ingredient) => {
-  const newItemEl = (document.createElement("li"));
+  const newItemEl = document.createElement("li");
   console.log(newItemEl);
   newItemEl.textContent = ingredient;
   newItemEl.classList.add("item");
